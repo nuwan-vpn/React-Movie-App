ff9f079c9a93e6ae8b558f2362505bd97b441457
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-05-18 22:34:34 +0530

    Add styled components for Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { GlobalStyle } from './GlobalStyle';
 
 function App() {
   return (
     <div className="App">
       Start here.
+      <GlobalStyle/>
     </div>
   );
 }
