4c0dc411153385c57e5cc3fadf4fd484c298d142
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-05-28 10:32:04 +0530

    feat: add Home component to App
    - Integrated Home component into the main App component
    - Placed Home component below Header and above the main content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { GlobalStyle } from './GlobalStyle';
 import Header from './components/Header/index';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Home/>
       Start here.
       <GlobalStyle/>
     </div>
