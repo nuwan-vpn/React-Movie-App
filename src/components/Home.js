4bbb89e16e4a86704a768a3676dcc3c2fe8f1a53
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-06-04 15:11:34 +0530

    feat: implement useHomeFetch custom hook
    - Create useHomeFetch custom hook to manage state and fetch logic for movies
    - Use useState to manage loading, error, and movie data state
    - Utilize useEffect hook to fetch initial movies on component mount
    - Export the state, loading, and error values from the hook
    
    The useHomeFetch hook encapsulates the logic for fetching movies from the API and managing the associated state. It initializes the state with default values and provides functions to update the loading and error states. The fetchMovies function fetches movies from the API and updates the state accordingly. The hook returns the current state, loading, and error values, which can be consumed by other components.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,13 @@
 import React, { useState, useEffect } from 'react';
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
-//API
-import API from '../API';
+
+import { useHomeFetch } from '../hooks/useHomeFetch';
 
 const Home = () => {
-  const [state, setState] = useState();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-
-  const fetchMovies = async (page, searchTerm = '') => {
-    try {
-      setError(false);
-      setLoading(true);
-      const movies = await API.fetchMovies(searchTerm, page);
-      setState(movies);
-    } catch (error) {
-      setError(true);
-    }
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    fetchMovies(1);
-  }, []);
-
-  console.log(state);
+ 
+  const { state, loading, error} = useHomeFetch()
+ // console.log(state);
 
   return <div>Home Page</div>;
 };
